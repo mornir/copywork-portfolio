e7a04a5af7aa271fc27a4c34489b457b656b1b8a
mornir <jeromepott@hotmail.com>
2020-07-12 12:13:26 +0200

    Update dummy content Make difference with draft content

--- plugins/preview.client.js
+++ plugins/preview.client.js
@@ -2,10 +2,10 @@ export default async function({ query, enablePreview }) {
   if (!query.preview) return
   console.log('preview!')
   enablePreview({
-    codepen: 'xyMPYj',
-    color: '#32a852',
-    copiedURL: 'https://nuxtjs.org/blog/going-full-static/',
-    title: 'Draft Content',
+    codepen: 'ZEQpNoL',
+    color: '#4287f5',
+    copiedURL: 'https://nuxtjs.org/',
+    title: 'Preview Content',
     video:
       'https://res.cloudinary.com/infonuagique/video/upload/v1543310621/copywork/LogRocket/logrocket-pricing-page',
   })
